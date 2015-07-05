90cb24c54bda9629e9ebc8eabb99a367a3bc3749
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2015-07-05 23:19:48 +0200

    First code commit
    basic functionality works. File is chosen via app.config for now

---
+++ PdfPresenter/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Windows;
+
+namespace PdfPresenter
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    [STAThread]
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      try
+      {
+        string path = ConfigurationManager.AppSettings["path"];
+
+        var currentSlide = new Slideshow(path, 0);
+        var nextSlide = new Slideshow(path, 1);
+        var mainSlideshow = new Slideshow(path);
+
+        mainSlideshow.ReportSlideChangesTo(currentSlide);
+        mainSlideshow.ReportSlideChangesTo(nextSlide);
+
+
+        var helper = new HelperWindow(
+          currentSlide, 
+          nextSlide
+        );
+
+        var mainWindow = new MainWindow(mainSlideshow);
+        mainWindow.Show();
+
+        helper.Owner = mainWindow;
+        helper.Show();
+
+        mainWindow.FocusOnPdf();
+
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message + " The application will exit now.");
+        throw;
+        //Shutdown(-1);
+      }
+
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+      base.OnExit(e);
+    }
+  }
+}
+
+
+
